071323dba431a653ff7ded9b57e893d31bdc8831
ismailalade <ismailalade1992@gmail.com>
2022-12-13 16:34:36 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - rint alphabets in lower and upper
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha:
+	for (alpha = 'a'; alphan <= 'z'; alpha++)
+		putcher(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
