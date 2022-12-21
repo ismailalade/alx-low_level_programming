055b067547b07b66dae56996825a8692fda64a92
ismailalade <ismailalade1992@gmail.com>
2022-12-21 13:27:03 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,8 +10,9 @@
 char *string_toupper(char *str)
 
 {
+
 	int index = 0;
-	
+
 	while (str[index])
 	{
 	if (str[index] >= 'a' && str[index] <= 'z')
@@ -19,4 +20,5 @@ char *string_toupper(char *str)
 	index++;
 	}
 	return (str);
+
 }
