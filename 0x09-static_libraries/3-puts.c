e5e2a818b2a265d2b59bf81028d2f24a1b73c7e4
ismailalade <ismailalade1992@gmail.com>
2023-01-07 15:08:41 -0800

    my task

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,14 @@
 #include "main.h"
-#include<stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * _puts - prints a string
+ * @str: string to print
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
